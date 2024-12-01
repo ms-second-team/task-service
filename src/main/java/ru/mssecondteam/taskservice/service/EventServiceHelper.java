cdb0627c768ede30bb6d4dac766649736d6ff51f
vvbakhanovich <vv.bakh@yandex.ru>
2024-12-01 09:38:06 +0300

    refactor: Change List to Set

--- src/main/java/ru/mssecondteam/taskservice/service/EventServiceHelper.java
+++ src/main/java/ru/mssecondteam/taskservice/service/EventServiceHelper.java
@@ -7,7 +7,7 @@ import ru.mssecondteam.taskservice.dto.event.EventDto;
 import ru.mssecondteam.taskservice.dto.event.TeamMemberDto;
 import ru.mssecondteam.taskservice.exception.NotAuthorizedException;
 
-import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,16 +18,16 @@ public class EventServiceHelper {
 
     public void checkIfEventExistsAndUsersAreEventTeamMembers(Long userId, Long eventId, Long teamMemberId) {
         final EventDto event = eventClient.getEventById(userId, eventId).getBody();
-        final List<Long> eventTeamMembersId = eventClient.getTeamsByEventId(userId, eventId).getBody().stream()
+        final Set<Long> eventTeamMembersId = eventClient.getTeamsByEventId(userId, eventId).getBody().stream()
                 .map(TeamMemberDto::userId)
-                .collect(Collectors.toList());
+                .collect(Collectors.toSet());
         eventTeamMembersId.add(event.ownerId());
 
         checkIfUserIsATeamMember(eventTeamMembersId, userId, eventId);
         checkIfUserIsATeamMember(eventTeamMembersId, teamMemberId, eventId);
     }
 
-    private void checkIfUserIsATeamMember(List<Long> teamMembersIds, Long userId, Long eventId) {
+    private void checkIfUserIsATeamMember(Set<Long> teamMembersIds, Long userId, Long eventId) {
         if (!teamMembersIds.contains(userId)) {
             throw new NotAuthorizedException(String.format("User is with id '%s' not a team member for event with id '%s'",
                     userId, eventId));
